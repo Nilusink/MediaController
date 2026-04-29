2146670570458fa67bf549c1e54ee217ed653921
Nilusink <nilusink@protonmail.com>
2026-04-29 23:10:45 +0200

    working (needs cleanup)

--- media_bridge.py
+++ media_bridge.py
@@ -9,16 +9,22 @@ Nilusink
 """
 from winrt.windows.media.control import GlobalSystemMediaTransportControlsSessionManager as MediaManager
 from winrt.windows.storage.streams import DataReader, Buffer, InputStreamOptions
+from PIL import Image, ImageDraw
 from io import BytesIO
-from PIL import Image
-import pygame as pg
 import typing as tp
-import threading
 import asyncio
+import serial
+import struct
 import time
-import math
 
 
+PORT = "COM13"        # change to your port (Linux: /dev/ttyUSB0)
+BAUD = 115200
+W, H = 80, 80
+
+
+def rgb888_to_565(r, g, b):
+    return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3)
 
 
 async def get_thumbnail() -> None | tuple[tp.Any, str, str, str]:
@@ -78,64 +84,103 @@ async def get_thumbnail() -> None | tuple[tp.Any, str, str, str]:
         )
 
 
-def get_pg_surf(image: Image) -> pg.Surface:
-    """get surface from thumbnail"""
-    mode = image.mode
-    size = image.size
-    data = image.tobytes()
+class ESPComm:
+    _sep = "<|>"
+    
+    def __init__(self, port: str = PORT, baud: int = BAUD):
+        self.ser = serial.Serial(port, baud, timeout=1)
+        time.sleep(2)
 
-    return pg.image.fromstring(data, size, mode)
+    def update_info(self, artist: str, title: str, album: str) -> bool:
+        """
+        update song info
+        :returns: True if successful, False otherwise
+        """
+        msg = f"INFO{self._sep}{artist}{self._sep}{title}{self._sep}{album}"
+        self.ser.write((msg + "\n").encode())
+        
+        return True
+    
+    def update_progress(self, position: float, duration: float, status: int) -> bool:
+        """
+        update progress bar
+        """
+        msg = f"PRG{self._sep}{position}{self._sep}{duration}{self._sep}{status}"
+        self.ser.write((msg + "\n").encode())
 
+        return True
 
-def circular_crop(surface, radius):
-    size = surface.get_size()
+    def update_thumbnail(self, thumbnail) -> bool:
+        """updates the thumbnail"""
+        # clear buff
+        self.clear_buff()
+        
+        # convert to circle
+        thumbnail = thumbnail.resize((H, W)).convert("RGB")
 
-    # create same-size alpha mask
-    mask = pg.Surface(size, pg.SRCALPHA)
-    mask.fill((0, 0, 0, 0))
+        mask = Image.new("L", (H, W), 0)
+        draw = ImageDraw.Draw(mask)
 
-    pg.draw.circle(mask, (255, 255, 255, 255), (radius, radius), radius)
+        draw.ellipse((0, 0, H - 1, W - 1), fill=255)
+        
+        bg = Image.new("RGB", (H, W), (0, 0, 0))  # or TFT background color
+        thumbnail = Image.composite(thumbnail, bg, mask)
+        
+        data = bytearray()
 
-    # copy original surface
-    result = surface.copy()
-    result.blit(mask, (0, 0), special_flags=pg.BLEND_RGBA_MULT)
+        for y in range(H):
+            for x in range(W):
+                pixel = thumbnail.getpixel((x, y))
+                r, g, b = pixel[0], pixel[1], pixel[2]
+                color = rgb888_to_565(r, g, b)
+                data += struct.pack("<H", color)  # big endian
+        
+        # send image
+        self.ser.write(f"IMG{self._sep}\n".encode())
+        self.ser.write(data)
 
-    return result
+        time.sleep(.1)
+        
+        # wait for response
+        response = self.ser.readline().decode(errors="ignore").strip()
+        if response:
+            return response == "OK"
+        
+        return True
 
+    def cls(self) -> None:
+        """clear screen"""
+        self.clear_buff()
 
-def darken(surface, factor=0.5):
-    overlay = pg.Surface(surface.get_size())
-    overlay.fill((0, 0, 0))
-    overlay.set_alpha(255*factor)
+        self.ser.write(f"CLS{self._sep}\n".encode())
 
-    surface.blit(overlay, (0, 0))
-    return surface
+        time.sleep(.1)
+        
+        # wait for response
+        response = self.ser.readline().decode(errors="ignore").strip()
+        if response:
+            return response == "OK"
+        
+        return True
 
+    def clear_buff(self):
+        """clear UART in buffer"""
+        while self.ser.in_waiting > 0:
+            response = self.ser.readline().decode(errors="ignore").strip()
+            # if response:
+            #     print("response: ", response)  # process commands here
 
-def fast_blur(surface, scale=0.25):
-    w, h = surface.get_size()
+            time.sleep(.1)
 
-    small = pg.transform.smoothscale(surface, (int(w * scale), int(h * scale)))
-    return pg.transform.smoothscale(small, (w, h))
-
-
-CENTER_SIZE = 80, 80
-CENTER_OFFSET = 120-(CENTER_SIZE[0]/2), 120-(CENTER_SIZE[1]/2)
-thumbnail, artist, title, a_title, pos, dur, status = None, None, None, None, 0, 0, None
-last_update = time.time()
-UPDATE_INTERVAL = 2.0  # seconds
-HALO_SIZE = 1
-running = True
-
-loop = asyncio.new_event_loop()
-asyncio.set_event_loop(loop)
-
-pg.init()
 
 def get_thumb_thread():
-    global thumbnail, artist, title, a_title, running, pos, dur, last_update, status
+    comm = ESPComm()
+    time.sleep(2)
 
-    while running:
+    pos = -1
+    
+    last_title = ""
+    while True:
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         res = loop.run_until_complete(get_thumbnail())
@@ -144,135 +189,29 @@ def get_thumb_thread():
 
             if new_pos != pos:
                 pos = new_pos
-                last_update = time.time()
+                comm.update_progress(pos, dur, status)
     
+            if title != last_title:
+                last_title = title
+
+                # draw info first
+                comm.cls()
+
+                # continue
+                comm.update_progress(pos, dur, status)
+                comm.update_info(artist, title, a_title)
+
+                # draw thumbnail
+                comm.update_thumbnail(thumbnail)
+
+                # update text again for it to be visible
+                comm.update_info(artist, title, a_title)
+    
+        # print awaiting messages
+        comm.clear_buff()
+
         time.sleep(1)
 
-threading.Thread(target=get_thumb_thread, daemon=True).start()
 
-screen = pg.display.set_mode((240, 240))
-pg.display.set_caption("Now Playing")
-
-_fonts = [
-    pg.font.SysFont(None, 24),
-    pg.font.SysFont(None, 20),
-    pg.font.SysFont(None, 16),
-    pg.font.SysFont(None, 12),
-    pg.font.SysFont(None, 8),
-]
-
-def render_text(
-        text: str,
-        color: tuple[float, float, float] | tuple[float, float, float, float],
-        starting_size: int = 0,
-        max_len: int = 200
-        ) -> pg.Surface:
-    """dynamically render a text based on size"""
-    for i in range(starting_size, len(_fonts)):
-        _rendered = _fonts[i].render(text, True, color)
-
-        if _rendered.get_size()[0] < max_len:
-            return _rendered
-    
-    else:
-        return _rendered
-
-
-def render_prog_bar(t: float, length: int, height: int = 10, status=4) -> pg.Surface:
-    """sine-wave progress bar with time offset"""
-
-    surf = pg.Surface((length, height), pg.SRCALPHA)
-    center_y = height / 2
-
-    if status != 4:
-        pg.draw.line(surf, (255, 255, 255), (0, center_y), (length * t, center_y))
-        pg.draw.line(surf, (128, 128, 128), (length * t, center_y), (length, center_y))
-        return surf
-
-    amp = height * .20  # wave amplitude
-    freq = 4             # number of waves along bar
-    speed = 1.0          # animation speed
-
-    phase = time.time() * speed
-
-    # progress split point
-    split = center_y + int((length - height) * t)
-
-    points_left = []
-    points_right = []
-    point_pos = 0, 0
-
-    for x in range(int(center_y), int(length - center_y)):
-        y = center_y + math.sin((x / length) * freq * 2 * math.pi + phase) * amp
-
-        if x <= split:
-            points_left.append((x, y))
-
-        else:
-            points_right.append((x, y))
-
-    point_pos = split,  center_y + math.sin((split / length) * freq * 2 * math.pi + phase) * amp
-
-
-    if len(points_left) > 1:
-        pg.draw.lines(surf, (255, 255, 255), False, points_left, 2)
-
-    if len(points_right) > 1:
-        pg.draw.lines(surf, (128, 128, 128), False, points_right, 2)
-
-    if point_pos[1] > 0:
-        pg.draw.circle(surf, (255, 255, 255), point_pos, height / 3)
-
-    return surf
-
-
-title_surface = None
-artist_surface = None
-a_title_surface = None
-
-running = True
-while running:
-    now = time.time()
-    screen.fill((50, 50, 50))
-
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
-
-    if thumbnail:
-        title_surface = render_text(title, (255, 255, 255), max_len=160)
-        artist_surface = render_text(artist,(200, 200, 200), 1)
-        a_title_surface = render_text(a_title, (180, 180, 180), 1)
-
-        # apply media stuff
-        surface = get_pg_surf(thumbnail.resize((240, 240)))
-        surface = pg.transform.smoothscale(surface, (240, 240))
-        surface = darken(fast_blur(circular_crop(surface, 120),), .6)
-
-        surface2 = get_pg_surf(thumbnail.resize(CENTER_SIZE))
-        surface2 = circular_crop(surface2, CENTER_SIZE[0]/2)
-
-        surface1 = pg.Surface((CENTER_SIZE[0] + 2*HALO_SIZE, CENTER_SIZE[1] + 2*HALO_SIZE), pg.SRCALPHA)
-        pg.draw.circle(surface1, (150, 150, 150, 40), (CENTER_SIZE[0]/2 + HALO_SIZE, CENTER_SIZE[1]/2 + HALO_SIZE), CENTER_SIZE[1]/2 + HALO_SIZE)
-
-        screen.blit(surface, (0, 0))
-        screen.blit(surface1, (CENTER_OFFSET[0]-HALO_SIZE, CENTER_OFFSET[1]-HALO_SIZE))
-        screen.blit(surface2, CENTER_OFFSET)
-
-        # draw texts
-        screen.blit(title_surface, ((240-title_surface.get_size()[0])/2, 30))
-        screen.blit(a_title_surface, ((240-a_title_surface.get_size()[0])/2, 50))
-        screen.blit(artist_surface, ((240-artist_surface.get_size()[0])/2, 170))
-
-        if status == 4:
-            t = min((pos + now-last_update) / dur, 1)
-        
-        else:
-            t = pos / dur
-
-        screen.blit(render_prog_bar(t, 160, 20, status=status), (40, 195))
-
-    pg.display.flip()
-
-pg.quit()
-running = False
+if __name__ == "__main__":
+    get_thumb_thread()
